2d1278920199bbfd033420efc00d8f3e4750a010
Amram1997 <amramananyan9@gmail.com>
2022-04-03 00:24:09 +0400

    update home and single page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./pages/Login";
-import {
-  createContext,
-  useEffect,
-  useLayoutEffect,
-  useReducer,
-  useState,
-} from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { useLayoutEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import { Profile } from "./pages/profile";
 import { Home } from "./pages/Home";
 import ProtectedRoute from "./helpers/ProtectedRoute";
